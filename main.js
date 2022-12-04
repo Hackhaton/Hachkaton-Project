30e9ae71591ec26fc72d80cd82b2eea08943e411
Lashvardi <lashakakashvili114@gmail.com>
2022-12-03 19:18:21 -0800

    Logic Push

--- main.js
+++ main.js
@@ -36,28 +36,55 @@ $(daily).click(function (e) {
 
 });
 
+$(todoes).click(function (e) { 
+  e.preventDefault();
+  append = `#Todo`
+
+});
+
+
+
 
 
 
 $(addtodo).click(function (e) { 
+  let todoname = $(saxel).val();
+  let descr = $(desc).val();
+  $(desc).val("");
+  $(saxel).val("");
   e.preventDefault();
   $(append).append(`        <div class="Todo_item">
   <div class="Add_button">
-    <div class="plus_box">
+    <div id="damateba" class="plus_box">
       <img class="Cross" src="img/X.svg" alt="">
     </div>
 
   </div>
   <div class="center_content">
-    <p class="description">Drink Water</p>
-
+    <p class="description">${todoname}</p>
+    <p class="patara">${descr}</p>
   </div>
   <div class="minus_button">
-    <div class="minus_box">
-      <img class="minus" src="img/minus.svg" alt="">
+    <div id="washla" class="minus_box">
+      <img  class="minus" src="img/minus.svg" alt="">
     </div>
   </div>
 
 </div>`);
+
+$(washla).click(function (e) { 
+  e.preventDefault();
+
+  $(this).closest(".Todo_item").remove();
   
 });
+let gemnum = 0;
+
+$(damateba).click(function (e) { 
+  e.preventDefault();
+  gemnum += 0.34;
+  $(Gem1).html(Math.round(gemnum * 100) / 100);
+});
+  
+});
+
